7da927ca1c0e21d6e64044492b60d5a20fd6dbe1
Dmitriy <d94-panov@mail.ru>
2025-02-06 20:41:24 +0500

    fix

---
+++ backend/Board/src/main/java/Backend/Board/BoardApplication.java
@@ -0,0 +1,28 @@
+package Backend.Board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class BoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@SuppressWarnings("null")
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200").allowedMethods("GET", "POST", "PUT",
+						"DELETE");
+			}
+		};
+	}
+}
