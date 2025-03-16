4f301fc6fda192d7766d96acc03b833b1aa35cc9
Dmitriy <d94-panov@mail.ru>
2025-03-16 16:36:41 +0500

    Review fix

--- backend/Board/src/main/java/Backend/Board/config/SecurityConfig.java
+++ backend/Board/src/main/java/Backend/Board/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
